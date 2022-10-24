da1b85057fe92eef0c92d9a4e8aff161af17ad78
AliceAgrest <alice.agrest@gmail.com>
2022-10-24 13:46:49 +0300

    addition

--- Program.cs
+++ Program.cs
@@ -10,43 +10,45 @@ namespace proj2
             int num, rnd1 , a ,b ,c ,d ,a2 ,b2 , c2 ,d2 , bul = 0 ,pgia = 0;
             Random rnd = new Random();
             rnd1 = rnd.Next(1000, 10000);
-            // 1234
-            // 
             a2 = rnd1 / 1000;
             b2 = (rnd1 / 100) % 10;
             c2 = (rnd1 / 10) % 10;
             d2 = rnd1 % 10;
-            Console.WriteLine("Enter  a four digit number ");
-            num = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter  a four digit number ");
+            //num = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < 10; i++)
             {
+                Console.WriteLine("Enter  a four digit number ");
+                num = int.Parse(Console.ReadLine());
                 a = num / 1000;
-                b = (num / 1000) % 100;
-                c = (num / 100) % 10;
-                d = (num / 1000) % 10;
+                b = (num / 100) % 10;
+                c = (num / 10) % 10;
+                d = num % 10;
 
-                if (a == a2)
+                if (a == a2 || a == b2 || a == c2 || a == d2)
                     bul += 1;
                 else
                     pgia += 1;
 
-                if (b == b2)
+                if (b == b2 || b == a2 || b == c2 || b == d2)
                     bul += 1;
                 else
                     pgia += 1;
 
-                if (c == c2)
+                if (c == c2 || c == a2 || c == b2 || c == d2)
                     bul += 1;
                 else
                     pgia += 1;
 
-                if (d == d2)
+                if (d == d2 || d == a2 || d == b2 || d == c2)
                     bul += 1;
                 else
                     pgia += 1;
+
+                Console.WriteLine($"You have {bul} buls and {pgia} pgia ");
             }
-            Console.WriteLine($"You have {bul} bols and {pgia} pgia ");
+            //Console.WriteLine($"You have {bul} buls and {pgia} pgia ");
 
             Console.ReadLine();
         }
